58ddcc354bdfa35f414a3f257f57f026fe91d6b8
gggiiina <a94128@gmail.com>
2025-07-30 11:08:56 +0800

    upd html css

--- libraries/__init__.py
+++
@@ -1,1 +0,0 @@
-# This file makes the 'libraries' directory a Python package.
--- static/js/liff-init.js
+++
@@ -1,8 +0,0 @@
-// static/js/liff-init.js
-async function initLiffAndGetUserId(liffId) {
-  if (!liff.isInitialized) {
-    await liff.init({ liffId: liffId });
-  }
-  const profile = await liff.getProfile();
-  return profile.userId;
-}
